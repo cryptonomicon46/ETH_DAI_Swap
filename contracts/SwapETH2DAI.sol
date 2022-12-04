5eddfcadf8142ced9ce3dc74bab660fc26a76c6c
cryptonomicon46 <sandman2145@gmail.com>
2022-12-04 10:15:45 -0800

    .exactInputSingle(params) function call fails

--- contracts/SwapETH2DAI.sol
+++ contracts/SwapETH2DAI.sol
@@ -2,125 +2,81 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity =0.7.6;
 pragma abicoder v2;
-
-import "hardhat/console.sol";
+// import "./UniSwap.sol";
 import "hardhat/console.sol";
 import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+ 
+interface IWETH is IERC20 {
+
+/// @notice deposit WETH9 from the sender
+function deposit() external payable;
+
+/// @notice withdraw WETH9 
+function withdraw(uint) external payable;
+
+}
 contract SwapETH2DAI {
     ISwapRouter public immutable swapRouter;
+    IWETH public immutable weth;
+    IERC20 public immutable dai;
+    uint24 constant poolFee = 3000;
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address constant ETH = 0x73bFE136fEba2c73F441605752b2B8CAAB6843Ec;
+    address constant SwapRouterAddress = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    // UniSwap private uni = new UniSwap();
+    event SwapCompleted(uint amountInETH,uint amountOutDAI);
+    event Received(address _sender, uint _value);
+    constructor() payable {
 
-    mapping (address => uint) private ETHBalanceOf;
-    mapping(address => uint) private DAIBalanceOf;
-    event TXIN_ETH(address sender, uint amount);
-    address public immutable DAI;
-    address public immutable ETH;
-    uint24 public constant poolFee = 3000;
-    event SwapETHForDai(uint amountInETH,uint amountOutDAI);
-    event WithDrawETH(uint ethAmount);
-    event WithDrawDAI(uint daiAmonut);
-    constructor(address DAI_,
-            address ETH_,
-              ISwapRouter _swapRouter) payable {
+        swapRouter = ISwapRouter(SwapRouterAddress);
+        weth = IWETH(WETH);
+        dai = IERC20(DAI);
+        }
 
-        DAI = DAI_;
-        ETH = ETH_;       
-        swapRouter = _swapRouter;
-    }
-    bool internal locked;
-    modifier noReentrance() {
-        require(!locked,"REENTRANCE");
-        locked = true;
-        _;
-        locked = false;
-    }
-
-    /// @notice swapETHforDai (ExactInputSingleHop) swaps amountInETH 
-    ///tokens to exact amountOutDAI
-    /// amountInETH should be less than owener's ETH Balance
-    /// owner's ETH balance should be greater than 0.1 ether
-    // function swapETHForDai(uint amountInETH) external payable returns (uint amountOutDAI) 
-      function swapETHForDai() external payable returns (uint amountOutDAI) 
-  {   
+    /// @notice swapETHforDai 
+    ///Owner transfers ETH as msg.value, the function wraps the needed ETH ->WETH9 
+    /// completes the swap operation and updates the owener's DAI balance
+    /// @dev emits event SWAP_COMPLETE(uint amountInETH,uint amountOutDAI) 
+    /// upon successful completion of the swap of ETH to DAI. 
+      function swapETHForDai() external payable {
         require(msg.value > 0.1 ether, "ETH_VALUE_TOO_LOW");
-        uint amountInETH = msg.value;
-        // require(msg.value <= ETHBalanceOf[msg.sender] && ETHBalanceOf[msg.sender]> 0.1 ether, "INVALID_SWAP_OPERATION");
-        console.log("amountInETH:", amountInETH);
+        require(msg.sender != address(0),"INVALID_SENDER_ADDRRESS");
 
-        // (bool success, ) = ETH.call(abi.encodeWithSelector(IERC20.approve.selector, address(swapRouter), amountInETH));
-        // require(success);
-        // console.log("Router contract Approval success");
-        console.log("ETH Balance before:",ETHBalanceOf[msg.sender]);
+        weth.deposit{value:msg.value }();
+        uint amountInWETH = IERC20(WETH).balanceOf(msg.sender);
+        IERC20(WETH).transferFrom(WETH,address(this),amountInWETH);
+        console.log("Balance of address(this)",IERC20(WETH).balanceOf(address(this)));
+        weth.approve(address(swapRouter), msg.value);
+        uint getRouterAllowance = IERC20(WETH).allowance(address(this),address(swapRouter));
+        console.log("Print Router's allowance",getRouterAllowance);
 
-        ETHBalanceOf[msg.sender] -= amountInETH;
-        console.log("ETH Balance after:",ETHBalanceOf[msg.sender]);
-
-        ISwapRouter.ExactInputSingleParams memory params = 
+           ISwapRouter.ExactInputSingleParams memory params = 
             ISwapRouter.ExactInputSingleParams({
-                tokenIn : ETH,
+                tokenIn : WETH,
                 tokenOut : DAI,
                 fee: poolFee,
                 recipient: msg.sender,
                 deadline: block.timestamp,
-                amountIn: amountInETH,
+                amountIn: amountInWETH,
                 amountOutMinimum: 0,
                 sqrtPriceLimitX96: 0
             });
            
-        console.log("params");
-
-        amountOutDAI = ISwapRouter(swapRouter).exactInputSingle(params);
-        console.log("amountOutDAI:", amountOutDAI);
-
-   
-        emit SwapETHForDai(amountInETH, amountOutDAI);
-
-        DAIBalanceOf[msg.sender] += amountOutDAI;        
+        uint amountOutDAI = ISwapRouter(swapRouter).exactInputSingle(params);
+        console.log("Swap completed and DAI received =", amountOutDAI);
+        // emit SwapCompleted(msg.value, amountOutDAI);
+        // console.log("DAI tokens received", amountOutDAI);
+        // // // //Transfer owner's DAI balances
+        // console.log("Sender's DAI bal after wrap:", IERC20(DAI).balanceOf(msg.sender));    
     }
 
+   /// @notice for empty calldata
+    receive() external payable {}
 
-    function withdrawETH() external payable noReentrance() {
-        require(ETHBalanceOf[msg.sender]>0, "ETH_BALANCE_ZERO");     
-        (bool success, ) = payable(msg.sender).call{value: ETHBalanceOf[msg.sender]}("");
-        require(success);
-        emit WithDrawETH(ETHBalanceOf[msg.sender]);
-        ETHBalanceOf[msg.sender] = 0;
-
-    }
-    
-    /// @notice withdraw DAI balance belonging to msg.sender if > 0 
-   function withdrawDAI() external payable noReentrance() {
-        require(DAIBalanceOf[msg.sender]>0, "DAI_BALANCE_ZERO");
-        (bool success, ) = payable(msg.sender).call{value: DAIBalanceOf[msg.sender]}("");
-        require(success);
-        emit WithDrawDAI(DAIBalanceOf[msg.sender]);
-        DAIBalanceOf[msg.sender] = 0;
-
-
-    }
-
-    /// @notice get owner's ETH balance
-   function getETHBalance() external view returns (uint eth_amount) {
-        eth_amount = ETHBalanceOf[msg.sender];
-    }
-
-    /// @notice get owner's DAI balance
-   function getDAIBalance() external view returns (uint dai_amonut) {
-        dai_amonut = DAIBalanceOf[msg.sender];
-    }
-
-    /// @notice receive function has to be delacred to receive ETH from a sender
-    /// @dev requires input value to be greater than 0.1ETH
-    /// emits TXIN_ETH(msg.sender, msg.value) upon sucess 
-    ///
-    receive() external payable {
-        require(msg.value > 0.1 ether,"INVALID_INPUT_ETH_VALUE");
-        ETHBalanceOf[msg.sender] += msg.value;
-        emit TXIN_ETH(msg.sender,msg.value);
-    }
-
-
-    
+    /// @notice when no other function matches, not even receive
+    fallback() external payable{}
     }
--- test/test_SwapETH2DAI.js
+++ test/test_SwapETH2DAI.js
@@ -7,13 +7,13 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { parse } = require("dotenv");
 
 
-const ETH_ADDRESS = "0x73bFE136fEba2c73F441605752b2B8CAAB6843Ec";
 const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const SwapRouterAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564"; 
 
 const DAI_DECIMALS = 18; 
-const SwapRouterAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564"; 
-console.log("ETH_ADDRESS:",ETH_ADDRESS);
 console.log("DAI_ADDRESS:", DAI_ADDRESS);
+console.log("WETH_ADDRESS:", WETH_ADDRESS);
 console.log("SWAP_ROUTER_ADDRESS:", SwapRouterAddress);
 
 const ercAbi = [
@@ -40,11 +40,13 @@ describe("SwapETH2DAI", function () {
 
     [owner,addr1,addr2] = await ethers.getSigners();
     const  SwapETH2DAI = await ethers.getContractFactory("SwapETH2DAI");
-    const swapETH2DAI = await SwapETH2DAI.deploy(DAI_ADDRESS,
-                                                        ETH_ADDRESS,
-                                                            SwapRouterAddress);
+    // const swapETH2DAI = await SwapETH2DAI.deploy(DAI_ADDRESS,
+    //                                                     WETH_ADDRESS,
+    //                                                         SwapRouterAddress);
+    const swapETH2DAI = await SwapETH2DAI.deploy();
     await swapETH2DAI.deployed();
-    const ETH = new ethers.Contract(ETH_ADDRESS, ercAbi, owner);
+    // const ETH = new ethers.Contract(ETH_ADDRESS, ercAbi, owner);
+    const WETH = new ethers.Contract(WETH_ADDRESS, ercAbi, owner);
     const DAI = new ethers.Contract(DAI_ADDRESS, ercAbi, owner);
 
     console.log("Owner:", owner.address);
@@ -52,123 +54,109 @@ describe("SwapETH2DAI", function () {
 
     console.log("DAI Contract Address:",DAI.address);
     console.log("SwapRouter Address:", )
-    return {swapETH2DAI, owner, ETH,DAI};
+    return {swapETH2DAI, owner, WETH,DAI};
     }   
 
 
   it("Check Contract deployed and initial ETH/DAI balances of owner!", async function () {
     
-    const {swapETH2DAI, owner,ETH,DAI} = await loadFixture(deploySimpleSwapFixture);
+    const {swapETH2DAI, owner,WETH,DAI} = await loadFixture(deploySimpleSwapFixture);
     expect(swapETH2DAI.address).to.be.a.properAddress;
 
-    const owner_ETH_bal = (await owner.getBalance());
+    const owner_ETH_bal_before = (await owner.getBalance());
 
     let owner_DAI_bal_before = await DAI.balanceOf(owner.address);
+    let owner_WETH_bal_before = await WETH.balanceOf(owner.address);
 
-    console.log("Owner: %s \n ETH_BAL: %o \n DAI_BAL:  %o",owner.address,
-                                            ethers.utils.formatEther(owner_ETH_bal),
-                                            ethers.utils.formatEther(owner_DAI_bal_before));
+
+
     // /* Check Initial ETH and DAI  Balances */ 
     const DAIBalanceBefore = Number(ethers.utils.formatUnits
         (owner_DAI_bal_before, 18))
 
-    const ETHBalanceBefore = Number(ethers.utils.formatUnits
-            (owner_ETH_bal, 18))
-    expect(DAIBalanceBefore).to.be.equal(parseEther("0"));
-    expect(ETHBalanceBefore).to.be.greaterThan(9000);
+    const WETHBalanceBefore = Number(ethers.utils.formatUnits
+            (owner_WETH_bal_before, 18))
 
+    const ETHBalanceBefore = Number(ethers.utils.formatUnits
+                (owner_ETH_bal_before, 18))
+    
+    expect(DAIBalanceBefore).to.be.equal(parseEther("0"));
+    expect(WETHBalanceBefore).to.be.equal(parseEther("0"));
+    expect(ETHBalanceBefore).to.be.greaterThan(9000);
+    console.log("Owner: %s \n ETH_BAL: %o \n DAI_BAL:  %o \n WETH_BAL:",owner.address,
+                                            ethers.utils.formatEther(owner_ETH_bal_before),
+                                            ethers.utils.formatEther(owner_DAI_bal_before),
+                                            ethers.utils.formatEther(owner_WETH_bal_before));
 
   });
 
-    it("Owner tries sending <0.1 ETH to contract, must revert and confirm all balances are still zero", async function () {
-        const {swapETH2DAI, owner,ETH,DAI} = await loadFixture(deploySimpleSwapFixture);
-
-    await expect(owner.sendTransaction({
-        to: swapETH2DAI.address,
-        value: parseEther("0.1"),
-      })).to.be.revertedWith("INVALID_INPUT_ETH_VALUE");
+    it("Owner tries to swap <0.1 ETH , operatation must revert", async function () {
+        const {swapETH2DAI, owner,WETH,DAI} = await loadFixture(deploySimpleSwapFixture);
     
-    const contract_bal = await ethers.provider.getBalance(swapETH2DAI.address);
-    //  console.log("Contract balance after transfer:", ethers.utils.formatEther(contract_bal));
-     expect(contract_bal).to.be.equal(parseEther('0'));
+        await expect(swapETH2DAI.swapETHForDai({ value: parseEther("0.1") })).
+        to.be.revertedWith("ETH_VALUE_TOO_LOW");
 
-     const ETHWithdrawal = await swapETH2DAI.connect(owner).getETHBalance();
-     const DAIithdrawal = await swapETH2DAI.connect(owner).getDAIBalance();
-
-    //  expect(ETHWithdrawal).to.be.equal(parseEther('0'));
-     expect(DAIithdrawal).to.be.equal(parseEther('0'));
-
+ 
     });
 
 
 
+    it("Owner tries to swap > 1 ETH to contract, swap must complete successfully", async function () {
+        const {swapETH2DAI, owner,WETH,DAI} = await loadFixture(deploySimpleSwapFixture);
+    
+        await swapETH2DAI.swapETHForDai({value: parseEther("10")});
+
+
+    // await expect(swapETH2DAI.swapETHForDai({ value: parseEther("1") })).
+    // to.emit(swapETH2DAI,"SwapCompleted");
+    
+
+ 
+    });
 
     it("Send 10 ETH to the contract and check ETH/DAI/contract balances", async function () {
-        const {swapETH2DAI, owner,ETH,DAI} = await loadFixture(deploySimpleSwapFixture);
+        const {swapETH2DAI, owner,WETH,DAI} = await loadFixture(deploySimpleSwapFixture);
 
 
-        await expect(() => owner.sendTransaction({to: swapETH2DAI.address, value: parseEther("10")}))
-        .to.changeEtherBalance(owner, parseEther("-10"));
+    //     await expect(() => owner.sendTransaction({to: swapETH2DAI.address, value: parseEther("10")}))
+    //     .to.changeEtherBalance(owner, parseEther("-10"));
         
-    const contract_bal = await ethers.provider.getBalance(swapETH2DAI.address);
-    //  console.log("Contract balance after transfer:", ethers.utils.formatEther(contract_bal));
+    // const contract_bal = await ethers.provider.getBalance(swapETH2DAI.address);
+    // //  console.log("Contract balance after transfer:", ethers.utils.formatEther(contract_bal));
 
-      const ETHBalance = await swapETH2DAI.connect(owner).getETHBalance();
-      const DAIBalance = await swapETH2DAI.connect(owner).getDAIBalance();
+    //   const ETHBalance = await swapETH2DAI.connect(owner).getETHBalance();
+    //   const DAIBalance = await swapETH2DAI.connect(owner).getDAIBalance();
 
 
-      expect(contract_bal).to.be.equal(parseEther('10'));
-    //   expect(ETHBalance).to.be.equal(parseEther('10'));
-      expect(DAIBalance).to.be.equal(parseEther('0'));
+    //   expect(contract_bal).to.be.equal(parseEther('10'));
+    // //   expect(ETHBalance).to.be.equal(parseEther('10'));
+    //   expect(DAIBalance).to.be.equal(parseEther('0'));
 
     });
 
 
-    it("Send ETH to contract and withdraw it and confirm withdraw event with args", async function () {
-        const {swapETH2DAI, owner,ETH,DAI} = await loadFixture(deploySimpleSwapFixture);
 
-        await owner.sendTransaction({
-            to: swapETH2DAI.address,
-            value: parseEther("10"),
-          });
-        
-
-          const ETHBalance = await swapETH2DAI.connect(owner).getETHBalance();
-          const DAIBalance = await swapETH2DAI.connect(owner).getDAIBalance();
-          const contract_bal = await ethers.provider.getBalance(swapETH2DAI.address);
-
-    
-          expect(contract_bal).to.be.equal(parseEther('10'));
-          expect(ETHBalance).to.be.equal(parseEther('10'));
-          expect(DAIBalance).to.be.equal(parseEther('0'));
-
-          await expect(swapETH2DAI.connect(owner).withdrawETH()).
-          to.emit(swapETH2DAI,"WithDrawETH").withArgs(ETHBalance);
-   
-
-
-    });
 
 
     it("Owner sends 10 ETH, Execute a swap on all 10 ETH, check Contract/ETH/DAI balances and withdrawal", async function () {
-        const {swapETH2DAI, owner,ETH,DAI} = await loadFixture(deploySimpleSwapFixture);
+        const {swapETH2DAI, owner,WETH,DAI} = await loadFixture(deploySimpleSwapFixture);
 
 
-        await expect(() => owner.sendTransaction({to: swapETH2DAI.address, value: parseEther("10")}))
-        .to.changeEtherBalance(owner, parseEther("-10"));
+        // await expect(() => owner.sendTransaction({to: swapETH2DAI.address, value: parseEther("10")}))
+        // .to.changeEtherBalance(owner, parseEther("-10"));
         
-        const contract_bal = await ethers.provider.getBalance(swapETH2DAI.address);
-         console.log("Contract balance after transfer:", ethers.utils.formatEther(contract_bal));
+        // const contract_bal = await ethers.provider.getBalance(swapETH2DAI.address);
+        //  console.log("Contract balance after transfer:", ethers.utils.formatEther(contract_bal));
 
-        const owner_DAI_bal_before = await swapETH2DAI.connect(owner).getDAIBalance();
+        // const owner_DAI_bal_before = await swapETH2DAI.connect(owner).getDAIBalance();
 
-            // // /* Execute the swap */
-        // await expect(ETH.connect(owner).approve(SwapRouterAddress,parseEther("10"))).
-        // to.emit(swapETH2DAI,"Approval");
+        //     // // /* Execute the swap */
+        // // await expect(ETH.connect(owner).approve(SwapRouterAddress,parseEther("10"))).
+        // // to.emit(swapETH2DAI,"Approval");
 
-        await ETH.approve(SwapRouterAddress,parseEther("10"));
-        // console.log(tx);
-        await swapETH2DAI.connect(owner).swapETHForDai({ value: ethers.utils.parseEther("10") })
+        // await ETH.approve(SwapRouterAddress,parseEther("10"));
+        // // console.log(tx);
+        // await swapETH2DAI.connect(owner).swapETHForDai({ value: ethers.utils.parseEther("10") })
 
         
         // // /* Test that we now have more DAI than when we started */
@@ -187,10 +175,10 @@ describe("SwapETH2DAI", function () {
         // expect(DAIBalanceAfter).is.greaterThan(DAIBalanceBefore);
     });
 
-    it("Owner sends 10 ETH, Execute a swap on 5 ETH, check Contract/ETH/DAI balances and withdrawal of ETH/DAI and ETH refund", async function () {});
+    // it("Owner sends 10 ETH, Execute a swap on 5 ETH, check Contract/ETH/DAI balances and withdrawal of ETH/DAI and ETH refund", async function () {});
 
 
-    it("Check for malicious contract trying to reenter and withdraw contract balance", async function () {});
+    // it("Check for malicious contract trying to reenter and withdraw contract balance", async function () {});
 
 
 
